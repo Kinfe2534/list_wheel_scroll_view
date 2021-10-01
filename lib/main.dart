485e3f731befddb89c9a0c273fef57b495244bb8
kinfe mulugeta <kinfe2534@gmail.com>
2021-10-01 16:46:31 +0300

    blah

--- lib/main.dart
+++ lib/main.dart
@@ -28,65 +28,91 @@ class DigitListWheelScrollView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> list = [];
-    for (int i = 0; i <= 9; i++) {
-      var scrollWheel = Padding(
-        padding: EdgeInsets.all(12.0),
-        child: Container(
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.1,
-                height: MediaQuery.of(context).size.height * 0.3,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.green,
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+    PageController controller =
+        PageController(viewportFraction: 1.0, initialPage: 2);
+    List<Widget> banners = <Widget>[];
+    List<Widget> row = <Widget>[];
+
+    for (int x = 0; x <= 9; x++) {
+      var bannerView = Padding(
+        padding: EdgeInsets.all(10),
+        child: CircleAvatar(
+          child: Container(
+            width: screenWidth / 5,
+            height: screenHeight * 0.2,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                CircleAvatar(
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
                           offset: Offset(4.0, 4.0),
-                          blurRadius: 4.0,
-                          spreadRadius: 3.0)
-                    ],
-                    gradient: LinearGradient(
+                          blurRadius: 5.0,
+                          spreadRadius: 1.0,
+                        )
+                      ])),
+                ),
+                CircleAvatar(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      gradient: LinearGradient(
                         begin: Alignment.topCenter,
                         end: Alignment.bottomCenter,
-                        colors: [Colors.white, Colors.blue])),
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    i.toString(),
-                    style: TextStyle(fontSize: 25, color: Colors.black),
+                        colors: [Colors.transparent, Colors.white],
+                      ),
+                    ),
                   ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Text(
+                            x.toString(),
+                            style:
+                                TextStyle(fontSize: 50.0, color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       );
-      list.add(scrollWheel);
+      banners.add(bannerView);
+    }
+    for (int i = 0; i < 5; i++) {
+      var oneCol = Container(
+        width: screenWidth / 5,
+        height: screenWidth / 5,
+        child: PageView(
+          controller: controller,
+          scrollDirection: Axis.vertical,
+          children: banners,
+        ),
+      );
+      row.add(oneCol);
     }
     return Container(
-      width: MediaQuery.of(context).size.width * 0.3,
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.1,
-        height: MediaQuery.of(context).size.height * 0.3,
-        child: ListWheelScrollView(
-          diameterRatio: 1,
-          physics: FixedExtentScrollPhysics(),
-          onSelectedItemChanged: (index) {
-            Fluttertoast.showToast(
-                msg: "Selected Item:${index}",
-                toastLength: Toast.LENGTH_SHORT,
-                gravity: ToastGravity.CENTER,
-                //  timeInSecForIos: 1,
-                backgroundColor: Colors.red,
-                textColor: Colors.white);
-          },
-          itemExtent: 15,
-          children: list,
-        ),
+      height: MediaQuery.of(context).size.height * 0.4,
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: row,
       ),
     );
   }
